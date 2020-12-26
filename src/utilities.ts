c19d41af8ba26ee2f88c56076fb0d22996777625
Gurveer Arora <garora1410@yahoo.co.uk>
2020-12-26 15:57:37 +0000

    create function to extract random subsets from arrays

---
+++ src/utilities.ts
@@ -0,0 +1,22 @@
+export function get_subsection_random_order<T>(options: T[], num: number): T[] {
+  if (num > options.length) {
+    console.error("Function called with number larger than array length");
+    return [];
+  }
+
+  const rand_nums = generate_n_random_numbers(num, 0, options.length);
+
+  return rand_nums.map((x) => options[x]);
+}
+
+function generate_n_random_numbers(
+  n: number,
+  min: number,
+  max: number
+): number[] {
+  const nums: Set<number> = new Set();
+  while (nums.size !== n) {
+    nums.add(Math.floor(Math.random() * (max - min)) + min);
+  }
+  return [...nums];
+}
